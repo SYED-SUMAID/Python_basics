ba17b475e8d32f4c49160d2b1c40b37bde1fb814
Sum <ddonation45@gmail.com>
2026-08-31 16:44:19 +0530

    Remove unwanted files

--- main.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-text = "Long live Verventech. Welcome to my Python project."
-
-tts = gTTS(text=text, lang="en")
-tts.save("hello.mp3")
-
-print("Audio created successfully!")
